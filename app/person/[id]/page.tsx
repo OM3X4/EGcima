a15af37d7d103433072931d6dec31b7f2df5cbcd
OM3X4 <om3x4e@gmail.com>
2025-07-03 18:12:23 +0300

    hi

--- app/person/[id]/page.tsx
+++ app/person/[id]/page.tsx
@@ -1,5 +1,4 @@
 export const dynamic = 'force-dynamic';
-
 import React from 'react'
 import { departmentFormatter, getImageUrl } from '@/app/utils/utils'
 import ArabicDate from '@/app/components/ArabicDate'
